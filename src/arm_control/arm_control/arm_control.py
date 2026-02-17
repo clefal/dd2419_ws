8b29146911dbc84b3df6df66f8e14801ac66e388
tarnvik <Tilde@tarnvik.com>
2026-02-17 15:59:47 +0100

    arm ready for initial integration

--- src/arm_control/arm_control/arm_control.py
+++ src/arm_control/arm_control/arm_control.py
@@ -2,15 +2,25 @@ import rclpy
 import time
 from rclpy.node import Node
 from robp_interfaces.msg import ArmControl
+from std_msgs.msg import String
 
 #TODO: make it faster??
 
 class Arm_control(Node):
     def __init__(self):
         super().__init__('arm_control')
-        self.pub = self.create_publisher(ArmControl, '/arm/control', 10)
         self.in_start_position = False
         self.holding_object = False
+        self.pub = self.create_publisher(ArmControl, '/arm/control', 10)
+
+        self.res = self.create_publisher(String, '/arm/result', 10)
+
+        self.subscription = self.create_subscription(
+            String,
+            '/arm/action',       
+            self.change_position,   
+            10                    
+        )
 
     def send_msg_start_position(self):
         msg = ArmControl()
@@ -101,6 +111,15 @@ class Arm_control(Node):
         self.pub.publish(msg)
         time.sleep(3.0)
 
+    def publish_res(self, data):
+        msg = String()
+        msg.data = data
+        self.res.publish(msg)
+
+    def start_position(self):
+        self.send_msg_start_position()
+        self.publish_res("START_SUCCESS")
+
     def pick_up_object(self):
         if (self.in_start_position):
             self.send_msg_raise_camera()
@@ -116,36 +135,38 @@ class Arm_control(Node):
             time.sleep(3.0)
 
             self.in_start_position = False
-            self.holding_object = True
-            #TODO: check that an object is in arm, and change self.holding_obejct property
+            self.holding_object = True #TODO: check that an object is actually in arm
+            self.publish_res("PICK_UP_SUCCESS")
             if (not self.holding_object):
-                #TODO: handle error 
+                self.publish_res("PICK_UP_FAIL_NO_OBJECT")
                 self.get_logger().error(f'Failed to pick up object: Object not in arm')
 
         else:
-            #TODO: handle error (Like tell task manager there was an error and to try again???)
+            self.publish_res("PICK_UP_FAIL_NO_START")
             self.get_logger().error(f'Can not initilize pick up: Arm not in start position')
-            self.send_msg_start_position()
 
     def drop_object(self):
         if (self.holding_object):
             self.send_msg_open_grip()
             self.holding_object = False
             self.send_msg_start_position()
+            self.publish_res("DROP_SUCCESS")
         else:
-            #TODO: handle error
+            self.publish_res("DROP_FAIL_NO_OBJECT")
             self.get_logger().error(f'Can not drop object: Not holdning an object')
+
+    def change_position(self, msg):    
+        if msg.data == "START":
+            self.start_position()
+        elif msg.data == "PICK_UP":
+            self.pick_up_object()
+        elif msg.data == "DROP":
+            self.drop_object()
 
 def main():
     rclpy.init()
     node = Arm_control()
-
-    #TODO: Listen to status topic 
-    
-    #TODO: When we get start message call node.send_msg_start_position()
-    #TODO: When we get arrived message call node.pick_up_object() 
-    #TODO: When we get drop message call node.drop_object()
-   
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == '__main__':
