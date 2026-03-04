40fe1784e54abe383a8a287f3fbb935897b85ed2
MariusGassmann <mgassmann@ethz.ch>
2026-03-04 19:49:24 +0100

    no camera

--- src/mapping/launch/workspace_loader.launch.py
+++ src/mapping/launch/workspace_loader.launch.py
@@ -81,21 +81,4 @@ def generate_launch_description():
             ]
         ),
 
-        # --- Static TF: base_link -> camera ---
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_to_camera',
-            arguments=[
-                '--x', '0.08987',
-                '--y', '0.0175',
-                '--z', '0.10456',
-                '--qx', '0.5',
-                '--qy', '-0.5',
-                '--qz', '0.5',
-                '--qw', '-0.5',
-                '--frame-id', 'base_link',
-                '--child-frame-id', 'realsense_camera_link'
-            ]
-        )
     ])
