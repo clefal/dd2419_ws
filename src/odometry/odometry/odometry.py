0daa834df2116a0050467bbbced9bc30ef116175
diegoosoriof <difaosfo@gmail.com>
2026-03-18 14:12:38 +0100

    Modify bias calc duration to 5 sec

--- src/odometry/odometry/odometry.py
+++ src/odometry/odometry/odometry.py
@@ -42,7 +42,7 @@ class Odometry(Node):
         self.declare_parameter('wheel_radius', 0.04921)
         self.declare_parameter('base', 0.308)
         self.declare_parameter('fix_tilt', True)
-        self.declare_parameter('gyro_bias_duration', 10.0)
+        self.declare_parameter('gyro_bias_duration', 5.0)
 
         # -------------------------
         # Robot model constants
