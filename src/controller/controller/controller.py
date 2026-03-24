99e1ce6fd78ed47b689c4c423f9880b4b3061f35
MariusGassmann <mgassmann@ethz.ch>
2026-03-24 18:42:35 +0100

    cont

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -81,7 +81,7 @@ class Controller(Node):
 
         # Parameters
         self.declare_parameter('lookahead_distance', 0.15)        # m
-        self.declare_parameter('nominal_linear_speed', 0.7)     # 0.35 duty-equivalent
+        self.declare_parameter('nominal_linear_speed', 0.9)     # 0.35 duty-equivalent
         self.declare_parameter('max_angular_speed', 0.3)        # 0.2 duty-equivalent
         self.declare_parameter('goal_tolerance', 0.08)  #0.05         # m
         self.declare_parameter('align_final_yaw', True)
