78d48a109be5422d5b34c8ca6f54c3289fd2dd33
tarnvik <Tilde@tarnvik.com>
2026-03-26 16:21:44 +0100

    test max pickup

--- src/arm_control/arm_control/arm_control.py
+++ src/arm_control/arm_control/arm_control.py
@@ -456,8 +456,7 @@ class Arm_control(Node):
 def main():
     rclpy.init()
     node = Arm_control()
-    node.initialize()
-    #test_pick_up_max_range(node)
+    test_pick_up_max_range(node)
     #test_pick_up_min_range(node)
     #test_drop_off(node)
     #test_full_pickup(node)
