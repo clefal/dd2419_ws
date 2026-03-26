cc99a33c8e8de0e90e148273eef821bbede87249
Clemens Faller <cfaller@kth.se>
2026-03-26 18:43:23 +0100

    dajusted final approach parameters

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -90,16 +90,16 @@ class Controller(Node):
 
 
         self.declare_parameter('min_linear_speed', 0.1)         # duty-equivalent (keep > deadzone margin)
-        self.declare_parameter('turn_gain', 0.2)                 # duty-per-rad for in-place turning
-        self.declare_parameter('control_period', 0.05)           # s (0.05=20Hz, 0.1=10Hz)
+        self.declare_parameter('turn_gain', 0.2)                # duty-per-rad for in-place turning
+        self.declare_parameter('control_period', 0.05)          # s (0.05=20Hz, 0.1=10Hz)
         self.declare_parameter('wheel_slew_rate', 1.5)          # duty/s max per-wheel change (except stop)
 
         self.declare_parameter('final_nominal_speed', 0.1)                # duty-equivalent for close approach
         self.declare_parameter('final_turn_gain', 0.8)                     # steering gain during close approach
         self.declare_parameter('final_max_angular_speed', 0.18)            # keep final approach conservative
         self.declare_parameter('final_turn_in_place_yaw_thresh', 0.35)     # rad
-        self.declare_parameter('final_stop_distance', 0.21)                 #0.17 gold
-        self.declare_parameter('final_lateral_offset', 0.00)                # m
+        self.declare_parameter('final_stop_distance', 0.23)                 # 0.17 gold
+        self.declare_parameter('final_lateral_offset', 0.5)                # m
         self.declare_parameter('final_target_timeout', 1.5)                # s
 
 
