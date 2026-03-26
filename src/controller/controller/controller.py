416ddd3d2cdb0eacffff3166b06f0c6c37295937
MariusGassmann <mgassmann@ethz.ch>
2026-03-26 14:43:59 +0100

    run

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -83,7 +83,7 @@ class Controller(Node):
         # Parameters
         self.declare_parameter('lookahead_distance', 0.15)        # m
         self.declare_parameter('nominal_linear_speed', 0.25)    # default slower for path tracking
-        self.declare_parameter('max_angular_speed', 0.2)        # cap turning a bit more conservatively
+        self.declare_parameter('max_angular_speed', 0.15)        # cap turning a bit more conservatively
         self.declare_parameter('goal_tolerance', 0.08)  #0.05         # m
         self.declare_parameter('align_final_yaw', True)
         self.declare_parameter('steering_gain', 0.1) #0.35
