91fffbcc883c7eba4c8facc1a92fed2b018851c6
diegoosoriof <difaosfo@gmail.com>
2026-04-12 18:32:07 +0200

    Mapping async lookup

--- src/mapping/mapping/mapping.py
+++ src/mapping/mapping/mapping.py
@@ -184,7 +184,20 @@ class Mapping(Node):
 
         self.skipped_scans = 0
         
-        #self.get_logger().info(f"{msg.header.frame_id}")
+        # Wait for the transform asynchronously
+        try: 
+            tf_future = self.tf_buffer.wait_for_transform_async(
+                "map",
+                msg.header.frame_id,
+                msg.header.stamp,
+            )
+        except TransformException as ex:
+            self.get_logger().warn(f"Async TF lookup failed: {ex}")
+            return
+
+        # Spin until transform found or `timeout_sec` seconds has passed
+        rclpy.spin_until_future_complete(self, tf_future, timeout_sec=1)
+
         try:
             tf = self.tf_buffer.lookup_transform(
                 "map",
