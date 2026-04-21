67cc800c74050bb2a0150c038c886673e087e939
Clemens Faller <cfaller@kth.se>
2026-04-21 15:23:12 +0200

    final_approach_start_delay_s to 1.5

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -103,7 +103,7 @@ class Controller(Node):
         self.declare_parameter('final_target_timeout', 1.5)                # s
 
         self._final_approach_enabled_wall = None
-        self.final_approach_start_delay_s = 0.5
+        self.final_approach_start_delay_s = 1.5
 
         # Motor deadzone requirement: each wheel is 0 or |duty| >= this
         self._dc_min = 0.08
