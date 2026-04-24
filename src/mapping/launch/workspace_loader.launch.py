77558e482456fe58dcf55778b2c0616152f655b8
diegoosoriof <difaosfo@gmail.com>
2026-04-24 18:48:59 +0200

    New workspace

--- src/mapping/launch/workspace_loader.launch.py
+++ src/mapping/launch/workspace_loader.launch.py
@@ -19,8 +19,8 @@ def generate_launch_description():
 
     pkg_share = get_package_share_directory('mapping')
 
-    workspace_file = _prefer_src_config('final_workspace4.csv', pkg_share)
-    map_file = _prefer_src_config('final_map4.csv', pkg_share)
+    workspace_file = _prefer_src_config('workspace_1.csv', pkg_share)
+    map_file = _prefer_src_config('map_1_1.csv', pkg_share)
 
     return LaunchDescription([
         DeclareLaunchArgument(
