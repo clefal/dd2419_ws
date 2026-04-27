ae8d17485a48a48d70e716d26be7567a7d6ff7c2
diegoosoriof <difaosfo@gmail.com>
2026-04-27 15:43:34 +0200

    Change map files

--- src/mapping/launch/workspace_loader.launch.py
+++ src/mapping/launch/workspace_loader.launch.py
@@ -19,8 +19,8 @@ def generate_launch_description():
 
     pkg_share = get_package_share_directory('mapping')
 
-    workspace_file = _prefer_src_config('final_workspace1.csv', pkg_share)
-    map_file = _prefer_src_config('final_map1.csv', pkg_share)
+    workspace_file = _prefer_src_config('final_workspace4.csv', pkg_share)
+    map_file = _prefer_src_config('final_map4.csv', pkg_share)
 
     return LaunchDescription([
         DeclareLaunchArgument(
