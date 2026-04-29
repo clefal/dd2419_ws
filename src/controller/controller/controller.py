4c6c767c0980a79152367dec1070b7d972fefabe
MariusGassmann <mgassmann@ethz.ch>
2026-04-29 15:20:55 +0200

    controller tuning

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -83,10 +83,10 @@ class Controller(Node):
         # Parameters
         self.declare_parameter('lookahead_distance', 0.19)        # m
         self.declare_parameter('nominal_linear_speed', 0.35)    # default slower for path tracking 0.25
-        self.declare_parameter('max_angular_speed', 0.17)        # cap turning a bit more conservatively 0.15
+        self.declare_parameter('max_angular_speed', 0.2)        # cap turning a bit more conservatively 0.15
         self.declare_parameter('goal_tolerance', 0.1)  #0.08        # m
         self.declare_parameter('align_final_yaw', True)
-        self.declare_parameter('steering_gain', 0.1) #0.35
+        self.declare_parameter('steering_gain', 0.06) #0.35
 
 
         self.declare_parameter('min_linear_speed', 0.1)         # duty-equivalent (keep > deadzone margin)
