7886effa1f5bdb2e3ec54e6ff6577428b3d5a7fd
MariusGassmann <mgassmann@ethz.ch>
2026-05-04 14:52:59 +0200

    controller spam

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -160,6 +160,9 @@ class Controller(Node):
             out_left = clamp(target_left, self._last_left_cmd - max_delta, self._last_left_cmd + max_delta)
             out_right = clamp(target_right, self._last_right_cmd - max_delta, self._last_right_cmd + max_delta)
 
+        if max(abs(out_left), abs(out_right)) > 0.30:
+            self.get_logger().info(f'High wheel duty: left={out_left:.3f}, right={out_right:.3f}')
+
         m = DutyCycles()
         m.duty_cycle_left = out_left
         m.duty_cycle_right = out_right
